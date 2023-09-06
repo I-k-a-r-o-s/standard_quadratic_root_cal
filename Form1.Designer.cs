26acfab0780df607cb519dddef2bbe7d9ed29f8b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2023-09-06 16:36:30 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace standard_quadratic_root_cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnNewProblem = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRoots = new System.Windows.Forms.Label();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSolve.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSolve.Location = new System.Drawing.Point(30, 246);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(75, 41);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // btnNewProblem
+            // 
+            this.btnNewProblem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnNewProblem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNewProblem.Location = new System.Drawing.Point(158, 246);
+            this.btnNewProblem.Name = "btnNewProblem";
+            this.btnNewProblem.Size = new System.Drawing.Size(75, 41);
+            this.btnNewProblem.TabIndex = 1;
+            this.btnNewProblem.Text = "New Problem";
+            this.btnNewProblem.UseVisualStyleBackColor = false;
+            this.btnNewProblem.Click += new System.EventHandler(this.btnNewProblem_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Location = new System.Drawing.Point(274, 246);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 41);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ax^2+bx+c=0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Coefficient a=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Coefficient b=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Coefficient c=";
+            // 
+            // lblRoots
+            // 
+            this.lblRoots.CausesValidation = false;
+            this.lblRoots.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoots.Location = new System.Drawing.Point(17, 193);
+            this.lblRoots.Name = "lblRoots";
+            this.lblRoots.Size = new System.Drawing.Size(357, 26);
+            this.lblRoots.TabIndex = 7;
+            this.lblRoots.Text = "Number of roots:";
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(274, 45);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(100, 20);
+            this.txt_a.TabIndex = 8;
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(274, 86);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(100, 20);
+            this.txt_b.TabIndex = 9;
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(274, 126);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(100, 20);
+            this.txt_c.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(400, 299);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.txt_b);
+            this.Controls.Add(this.txt_a);
+            this.Controls.Add(this.lblRoots);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewProblem);
+            this.Controls.Add(this.btnSolve);
+            this.Name = "Form1";
+            this.Text = "Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button btnNewProblem;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblRoots;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.TextBox txt_c;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace standard_quadratic_root_cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            try
+            {               
+                x = (-(double.Parse(txt_b.Text)) + Math.Sqrt((double.Parse(txt_b.Text) * double.Parse(txt_b.Text)) - 4 * (double.Parse(txt_a.Text)) * (double.Parse(txt_c.Text)))) / (2 * (double.Parse(txt_a.Text)));
+                y = (-(double.Parse(txt_b.Text)) - Math.Sqrt((double.Parse(txt_b.Text) * double.Parse(txt_b.Text)) - 4 * (double.Parse(txt_a.Text)) * (double.Parse(txt_c.Text)))) / (2 * (double.Parse(txt_a.Text)));
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid inputs","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }           
+            if(((double.Parse(txt_b.Text) * double.Parse(txt_b.Text)) - 4 * (double.Parse(txt_a.Text)) * (double.Parse(txt_c.Text)))>0)
+                {
+                lblRoots.Text = "Number of roots:2"+"  : "+x.ToString("0.0000")+" , "+y.ToString("0.0000");
+            }
+            if (((double.Parse(txt_b.Text) * double.Parse(txt_b.Text)) - 4 * (double.Parse(txt_a.Text)) * (double.Parse(txt_c.Text))) == 0)
+            {
+                lblRoots.Text = "Number of roots:1" + "  : " + x.ToString("0.0000");
+            }
+            if(((double.Parse(txt_b.Text) * double.Parse(txt_b.Text)) - 4 * (double.Parse(txt_a.Text)) * (double.Parse(txt_c.Text))) < 0)
+            {
+                lblRoots.Text = "Number of roots:None";
+            }
+        }
+
+        private void btnNewProblem_Click(object sender, EventArgs e)
+        {
+            lblRoots.Text = "Number of roots";
+            txt_a.Text = string.Empty;
+            txt_b.Text = string.Empty;
+            txt_c.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
